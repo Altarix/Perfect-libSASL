1cc62e887afae4cd1afcf757a7c62009cc77f044
Sergey Balalaev <sof.bix@mail.ru>
2019-03-26 14:29:38 +0400

    to spm4 refactory

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "SASL",
     providers: [
-          .Apt("libsasl2-dev")
+        .Apt("libsasl2-dev")
     ]
 )
